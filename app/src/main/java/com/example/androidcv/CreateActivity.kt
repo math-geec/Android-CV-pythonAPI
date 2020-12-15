918c5b3c3698fc030fba3dc324fa561ca6ceb7f5
math-geec <victordean087@gmail.com>
2020-12-15 23:52:31 +0000

    add CreateActivity

--- app/src/main/java/com/example/androidcv/CreateActivity.kt
+++ app/src/main/java/com/example/androidcv/CreateActivity.kt
@@ -1,11 +1,46 @@
 package com.example.androidcv
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_create.*
 
+private const val PICK_PHOTO_CODE = 42
 class CreateActivity : AppCompatActivity() {
+    private var photoUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+
+        btnGallary.setOnClickListener {
+            Log.i("btnGallery", "Open up image picker on device")
+            val imagePickerIntent = Intent(Intent.ACTION_GET_CONTENT)
+            imagePickerIntent.type = "image/*"
+            if (imagePickerIntent.resolveActivity(packageManager) != null) {
+                startActivityForResult(imagePickerIntent, PICK_PHOTO_CODE)
+            }
+
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // check the result is requested from the right intent
+        if (requestCode == PICK_PHOTO_CODE) {
+            // check the user has selected an image
+            if (resultCode == Activity.RESULT_OK) {
+                photoUri = data?.data
+                Log.i("btnGallery", "photoUri $photoUri")
+                imageView
+            } else {
+                Toast.makeText(this, "Image seletion canceled.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/androidcv/MainActivity.kt
+++ app/src/main/java/com/example/androidcv/MainActivity.kt
@@ -1,18 +1,17 @@
 package com.example.androidcv
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        fabCreate.setOnClickListener {
-            val intent = Intent(this, CreateActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
