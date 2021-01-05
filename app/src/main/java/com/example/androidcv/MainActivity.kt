40a9c386293ab2aea369b9f56452ee1c41b78082
math-geec <victordean087@gmail.com>
2021-01-05 00:04:25 +0000

    test api connection with python model

--- app/src/main/java/com/example/androidcv/MainActivity.kt
+++ app/src/main/java/com/example/androidcv/MainActivity.kt
@@ -7,7 +7,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.google.gson.JsonObject
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +23,58 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, CreateActivity::class.java)
             startActivity(intent)
         }
+
+        /*********************************/
+        // test api connection python model
+        btnPOST.setOnClickListener {
+            val jsonObj = JsonObject()
+            jsonObj.addProperty("title", "rhythm")
+            jsonObj.addProperty("artist", "meee")
+            jsonObj.addProperty("text", "DJ drop the beat!")
+            //  POST demo
+            APIKindaStuff
+                .service
+                .getVectors(jsonObj)
+                .enqueue(object : Callback<ResponseBody> {
+                    override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                        println("---TTTT :: POST Throwable EXCEPTION:: " + t.message)
+                    }
+
+                    override fun onResponse(
+                        call: Call<ResponseBody>,
+                        response: Response<ResponseBody>
+                    ) {
+                        if (response.isSuccessful) {
+                            val msg = response.body()?.string()
+                            println("---TTTT :: POST msg from server :: " + msg)
+                            Toast.makeText(applicationContext, msg, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                })
+        }
+
+        btnGET.setOnClickListener {
+            // GET demo
+            APIKindaStuff
+                .service
+                .greetUser("math-geec")
+                .enqueue(object : Callback<ResponseBody> {
+                    override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                        println("---TTTT :: GET Throwable EXCEPTION:: " + t.message)
+                    }
+
+                    override fun onResponse(
+                        call: Call<ResponseBody>,
+                        response: Response<ResponseBody>
+                    ) {
+                        if (response.isSuccessful) {
+                            val msg = response.body()?.string()
+                            println("---TTTT :: GET msg from server :: " + msg)
+                            Toast.makeText(applicationContext, msg, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                })
+        }
+        /*********************************/
     }
 }
--- app/src/main/java/com/example/androidcv/RetrofitAPI.kt
+++ app/src/main/java/com/example/androidcv/RetrofitAPI.kt
@@ -1,30 +1,55 @@
 package com.example.androidcv
 
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonObject
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.Multipart
-import retrofit2.http.POST
-import retrofit2.http.Part
+import retrofit2.http.*
 
-interface RetrofitAPI {
+// interface RetrofitAPI {
+//
+//     @Multipart
+//     @POST("Api.php?apicall=upload")
+//     fun uploadImage(
+//         @Part image: MultipartBody.Part,
+//         @Part("desc") desc: RequestBody
+//     ): Call<UploadResponse>
+//
+//     companion object {
+//         operator fun invoke(): RetrofitAPI {
+//             return Retrofit.Builder()
+//                 .baseUrl("http://10.10.10.118/ImageUploader/")
+//                 .addConverterFactory(GsonConverterFactory.create())
+//                 .build()
+//                 .create(RetrofitAPI::class.java)
+//         }
+//     }
+// }
 
-    @Multipart
-    @POST("Api.php?apicall=upload")
-    fun uploadImage(
-        @Part image: MultipartBody.Part,
-        @Part("desc") desc: RequestBody
-    ): Call<UploadResponse>
+/*********************************/
+// test api connection python model
+class APIKindaStuff {
+
+    interface APIService {
+        @GET("/users/{user}")
+        fun greetUser(@Path("user") user: String): Call<ResponseBody>
+
+        @Headers("Content-type: application/json")
+        @POST("/api/post_some_data")
+        fun getVectors(@Body body: JsonObject): Call<ResponseBody>
+    }
 
     companion object {
-        operator fun invoke(): RetrofitAPI {
-            return Retrofit.Builder()
-                .baseUrl("http://10.10.10.118/ImageUploader/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(RetrofitAPI::class.java)
-        }
+        private val retrofit = Retrofit.Builder()
+            .baseUrl("http://0.0.0.0:5000")
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .build()
+
+        var service = retrofit.create(APIService::class.java)
     }
 }
+/*********************************/
